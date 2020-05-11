3150f1ddb4931305ff7afc5eab338e99be38435c
Manuel Schmalstieg <git.code@ms-studio.net>
2020-05-11 08:42:58 +0200

    Charger les styles de la partie Membres

--- functions.php
+++ functions.php
@@ -7,6 +7,10 @@ function my_theme_enqueue_styles() {
     wp_enqueue_style( 
     	$parent_style, 
     	get_template_directory_uri() . '/style.css' );
+    	
+    wp_enqueue_style( 
+    	'membres', 
+    	get_stylesheet_directory_uri() . '/css/style-membres.css' );
 
 //    wp_enqueue_style( 'child-style',
 //        get_stylesheet_directory_uri() . '/style.css',
@@ -19,3 +23,5 @@ function my_theme_enqueue_styles() {
 
 
 require_once('functions/typekit.php');
+
+require_once('functions/pre-get.php');
